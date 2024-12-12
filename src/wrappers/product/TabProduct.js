c28916f48663b93f5a47109abfec47a18aaf1fc0
Your Name <dev.hashebur@gmail.com>
2024-12-13 04:00:45 +0600

    loader add tab product

--- src/wrappers/product/TabProduct.js
+++ src/wrappers/product/TabProduct.js
@@ -9,6 +9,7 @@ import Tab from "react-bootstrap/Tab";
 import { Link, useNavigate } from "react-router-dom";
 import Rating from "../../components/product/sub-components/ProductRating";
 import SectionTitle from "../../components/section-title/SectionTitle";
+import Spinner from "../../components/Spinner/Spinner";
 import { Base_Url } from "../../Config/config";
 
 export default function TabProduct() {
@@ -57,94 +58,100 @@ export default function TabProduct() {
         </Nav>
 
         <Tab.Content>
-          <Tab.Pane eventKey="bestSeller">
-            <Row xs={2} sm={3} md={4} lg={4} className="xs:g-2 g-2 h-75">
-              {limitedProducts.map((p) => (
-                <Col className={clsx("product-wrap")} key={p._id}>
-                  <Card onClick={() => handleClick(p._id, p.name)}>
-                    <div className="product-img img-fluid">
-                      <Link to={process.env.PUBLIC_URL + "/products/" + p._id}>
-                        {/* Display placeholder initially */}
-                        <img
-                          className="default-img"
-                          src={`${process.env.PUBLIC_URL}/shohojdokan-product-loader.png`}
-                          alt={p.name}
-                          style={{ display: "block" }}
-                        />
-                        {/* Once loaded, show the actual image */}
-                        <img
-                          className="default-img"
-                          src={p.image}
-                          alt={p.name}
-                          onLoad={(e) =>
-                            (e.currentTarget.previousSibling.style.display =
-                              "none")
-                          }
-                          style={{ display: "block" }}
-                        />
-                      </Link>
-                      {p.discount || p.new ? (
-                        <div className="product-img-badges">
-                          {p.discount ? (
-                            <span className="pink">-{p.discount}%</span>
-                          ) : (
-                            ""
-                          )}
-                          {p.new ? <span className="purple">New</span> : ""}
-                        </div>
-                      ) : (
-                        ""
-                      )}
-                    </div>
-
-                    <div className="product-content text-center mb-3">
-                      <h3>
+          {loading ? (
+            <Spinner />
+          ) : (
+            <Tab.Pane eventKey="bestSeller">
+              <Row xs={2} sm={3} md={4} lg={4} className="xs:g-2 g-2 h-75">
+                {limitedProducts.map((p) => (
+                  <Col className={clsx("product-wrap")} key={p._id}>
+                    <Card onClick={() => handleClick(p._id, p.name)}>
+                      <div className="product-img img-fluid">
                         <Link
                           to={process.env.PUBLIC_URL + "/products/" + p._id}
                         >
-                          {p.name.slice(0, 16)}
-                          {p.name.length > 16 ? "..." : ""}
+                          {/* Display placeholder initially */}
+                          <img
+                            className="default-img"
+                            src={`${process.env.PUBLIC_URL}/shohojdokan-product-loader.png`}
+                            alt={p.name}
+                            style={{ display: "block" }}
+                          />
+                          {/* Once loaded, show the actual image */}
+                          <img
+                            className="default-img"
+                            src={p.image}
+                            alt={p.name}
+                            onLoad={(e) =>
+                              (e.currentTarget.previousSibling.style.display =
+                                "none")
+                            }
+                            style={{ display: "block" }}
+                          />
                         </Link>
-                      </h3>
-                      {p.rating && p.rating > 0 ? (
-                        <div className="product-rating">
-                          <Rating ratingValue={p.rating} />
-                        </div>
-                      ) : (
-                        ""
-                      )}
-                      <div className="product-price">
-                        {p.price !== null ? (
-                          <Fragment>
-                            <span>
-                              ৳
-                              {Math.round(
-                                p.price - (p.price / 100) * p.discount
-                              )}
-                            </span>
-                            <span className="old">৳{p.price}</span>
-                          </Fragment>
+                        {p.discount || p.new ? (
+                          <div className="product-img-badges">
+                            {p.discount ? (
+                              <span className="pink">-{p.discount}%</span>
+                            ) : (
+                              ""
+                            )}
+                            {p.new ? <span className="purple">New</span> : ""}
+                          </div>
                         ) : (
-                          <span>৳{p.price} </span>
+                          ""
                         )}
                       </div>
-                    </div>
-                  </Card>
-                </Col>
-              ))}
-            </Row>
 
-            <div className="d-flex justify-content-center mt-2">
-              {limitedProducts.length > 0 &&
-                limitedProducts.length < fetchProduct.length && (
-                  <div className="d-flex justify-content-center mt-2">
-                    <button className="seemore" onClick={showMore}>
-                      Show More
-                    </button>
-                  </div>
-                )}
-            </div>
-          </Tab.Pane>
+                      <div className="product-content text-center mb-3">
+                        <h3>
+                          <Link
+                            to={process.env.PUBLIC_URL + "/products/" + p._id}
+                          >
+                            {p.name.slice(0, 16)}
+                            {p.name.length > 16 ? "..." : ""}
+                          </Link>
+                        </h3>
+                        {p.rating && p.rating > 0 ? (
+                          <div className="product-rating">
+                            <Rating ratingValue={p.rating} />
+                          </div>
+                        ) : (
+                          ""
+                        )}
+                        <div className="product-price">
+                          {p.price !== null ? (
+                            <Fragment>
+                              <span>
+                                ৳
+                                {Math.round(
+                                  p.price - (p.price / 100) * p.discount
+                                )}
+                              </span>
+                              <span className="old">৳{p.price}</span>
+                            </Fragment>
+                          ) : (
+                            <span>৳{p.price} </span>
+                          )}
+                        </div>
+                      </div>
+                    </Card>
+                  </Col>
+                ))}
+              </Row>
+
+              <div className="d-flex justify-content-center mt-2">
+                {limitedProducts.length > 0 &&
+                  limitedProducts.length < fetchProduct.length && (
+                    <div className="d-flex justify-content-center mt-2">
+                      <button className="seemore" onClick={showMore}>
+                        Show More
+                      </button>
+                    </div>
+                  )}
+              </div>
+            </Tab.Pane>
+          )}
         </Tab.Content>
       </Tab.Container>
     </div>
